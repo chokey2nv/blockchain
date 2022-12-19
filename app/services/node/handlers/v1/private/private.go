411094d93b9a9b377b7dbef7a74b079c424cfa3e
Ceejay <chokey2nv@gmail.com>
2022-12-19 07:35:50 +0100

    day 2 training

---
+++ app/services/node/handlers/v1/private/private.go
@@ -0,0 +1,24 @@
+// Package private maintains the group of handlers for node to node access.
+package private
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/chokey2nv/blockchain/foundation/web"
+	"go.uber.org/zap"
+)
+
+// Handlers manages the set of bar ledger endpoints.
+type Handlers struct {
+	Log *zap.SugaredLogger
+}
+
+func (h Handlers) SampleHander(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	status := struct {
+		Status string
+	}{
+		Status: "OK",
+	}
+	return web.Respond(ctx, w, status, http.StatusOK)
+}
---
+++ app/tooling/scratch/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func run() error {
+	path := fmt.Sprintf("%s%s.ecdsa", "zblock/accounts/", "kennedy")
+
+	privateKey, err := crypto.LoadECDSA(path)
+	if err != nil {
+		return fmt.Errorf("load: %w", err)
+	}
+	// privateKey, err := crypto.GenerateKey()
+	// fmt.Println(privateKey)
+	// if err != nil {
+	// 	return err
+	// }
+	address := crypto.PubkeyToAddress(privateKey.PublicKey).String()
+	fmt.Println(address)
+
+	v := struct {
+		Name string
+	}{
+		Name: "chijioke",
+	}
+
+	data, err := json.Marshal(v)
+	if err != nil {
+		return err
+	}
+
+	dataHash := crypto.Keccak256(data)
+
+	sig, err := crypto.Sign(dataHash, privateKey)
+	if err != nil {
+		return fmt.Errorf("sign: %w", err)
+	}
+	fmt.Println(sig)
+
+	signPublicKey, err := crypto.Ecrecover(dataHash, sig)
+	if err != nil {
+		return fmt.Errorf("recover pub key: %w", err)
+	}
+	fmt.Println(signPublicKey)
+
+	x, y := elliptic.Unmarshal(crypto.S256(), signPublicKey)
+	publicKey := ecdsa.PublicKey{
+		Curve: crypto.S256(),
+		X:     x,
+		Y:     y,
+	}
+	fmt.Println(crypto.PubkeyToAddress(publicKey))
+	return nil
+}
