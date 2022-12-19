f741d62e8086082a016911b5df80123dd8adc9d8
Ceejay <chokey2nv@gmail.com>
2022-12-19 08:47:11 +0100

    day2 - stamping and sig verification

--- app/tooling/scratch/main.go
+++ app/tooling/scratch/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
+	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 
@@ -48,15 +50,43 @@ func sign() error {
 	if err != nil {
 		return fmt.Errorf("sign: %w", err)
 	}
-	fmt.Println(sig)
+	fmt.Printf("sig: 0x%s\n", hex.EncodeToString(sig))
 
 	// =========================================
+
+	// hack the public key
+	v2 := struct {
+		Name string
+	}{
+		Name: "chijio", // WRONG CALCULATION WILL PRODUCE DIFFERENT ADDERSS (0xF01813E4B85e178A83e29B8E7bF26BD830a25f32 | 0xfEef90eE1cfba406a9d671CBEa76Cf666D321b58)
+	}
+
+	data2, err := json.Marshal(v2)
+	if err != nil {
+		return err
+	}
+
+	dataHash2, err := stamp(data2)
+	if err != nil {
+		return fmt.Errorf("stamp error: %w %w", err, dataHash2)
+	}
+
+	// hack ends
 
 	signPublicKey, err := crypto.Ecrecover(dataHash, sig)
 	if err != nil {
 		return fmt.Errorf("recover pub key: %w", err)
 	}
-	fmt.Println(signPublicKey)
+	// fmt.Println(signPublicKey)
+
+	//======== check hack =========
+	//dataHash generated sig not dataHash2
+	rs := sig[:crypto.RecoveryIDOffset]
+	if !crypto.VerifySignature(signPublicKey, dataHash2, rs) {
+		return errors.New("invalid signature")
+	}
+
+	//======
 
 	x, y := elliptic.Unmarshal(crypto.S256(), signPublicKey)
 	publicKey := ecdsa.PublicKey{
@@ -66,4 +96,22 @@ func sign() error {
 	}
 	fmt.Println(crypto.PubkeyToAddress(publicKey))
 	return nil
+}
+func stamp(value any) ([]byte, error) {
+
+	// Marshal the data.
+	v, err := json.Marshal(value)
+	if err != nil {
+		return nil, err
+	}
+
+	// This stamp is used so signatures we produce when signing data
+	// are always unique to the Ardan blockchain.
+	stamp := []byte(fmt.Sprintf("\x19Ardan Signed Message:\n%d", len(v)))
+
+	// Hash the stamp and txHash together in a final 32 byte array
+	// that represents the data.
+	data := crypto.Keccak256(stamp, v)
+
+	return data, nil
 }
